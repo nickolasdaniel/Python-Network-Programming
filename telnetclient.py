086e18403f9394532d8efa51e61e2f4517e4a250
ScriptBoy2000 <31825176+nickolasdaniel@users.noreply.github.com>
2018-02-21 23:02:20 +0200

    Update telnetclient.py

--- telnetclient.py
+++ telnetclient.py
@@ -7,7 +7,7 @@ def init_client(server_address):
     client_socket.connect(server_address)
 
     while connected:
-        allowed__commands = ['ls, cd, ls-l']
+        # allowed__commands = ['ls, cd, ls-l']
         try:
             if server_address[0] == '127.0.0.1':
                 message = input('\nlocalhost@localhost' + '>> ')
@@ -16,11 +16,30 @@ def init_client(server_address):
             print('Sending message: {}'.format(message))
             client_socket.sendall(message.encode('utf-8'))
 
-        except socket.error as e:
-            print('Socket error: {}'.format(e))
+            recv_data(client_socket)
+
+        except IOError as e:
+            print('Error: {}'.format(e))
+
         except Exception as e:
             print('Other error: {}'.format(e))
+
         except KeyboardInterrupt:
                 connected = False
+def recv_data(client_socket):
+    recv_size = 1
+    MAX_RECV_BUFFER = 1024
+    while recv_size:
+        data_buffer = client_socket.recv(MAX_RECV_BUFFER)
+        message = data_buffer.decode('utf-8')
+        print('Received data: '.format(message))
+
+        if not data_buffer:
+            break
+        recv_size += len(data_buffer)
+
+    data_buffer = ''
+    client_socket.close()
+
 if __name__ == '__main__':
     init_client(('127.0.0.1', 8080))
